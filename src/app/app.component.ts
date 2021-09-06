dae4a0e8f42a87875b56095fcb9da2e0d31d08a8
Abhishek Sharma <abhivsharma8@gmail.com>
2021-09-06 15:21:25 +0530

    Footer component with event to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'The Lazy Investor';
+  myHeaderTitle = 'The Lazy Investor';
+  myFooterTitle = 'Love Financial Tips ?';
   backGroundImagePath = '/assets/images/blue_sky.jpg';
+
+  openGitHubTab(open: boolean) {
+    if (open) {
+      window.open("https://github.com/MiHawkStackOverFlow/the-lazy-investor-angular-", "_blank");
+    }
+  }
 }
---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+  public footerTitle = '';
+  @Output() goToGithub = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  @Input()
+  set title(title: string) {
+    this.footerTitle = (title.toUpperCase()) || `Rate me on GitHub`;
+  }
+
+  ngOnInit(): void {  }
+
+  goToGitHubPage(value: boolean) {
+    this.goToGithub.emit(value);
+  }
+
+}
